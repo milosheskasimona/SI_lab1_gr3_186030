37dee86e0e9d5f92feee87782e81d02bd806cd39
milosheskasimona <63304675+milosheskasimona@users.noreply.github.com>
2020-04-07 22:30:10 +0200

    tax type variable added

---
+++ Item.java
@@ -0,0 +1,12 @@
+class Item {
+	int id;
+	String name;
+	double price;
+	//TODO add variable.
+	char danok;
+	//TODO constructor
+	//TODO setters and getters
+	double taxReturn () {
+		//TODO
+	}
+}
